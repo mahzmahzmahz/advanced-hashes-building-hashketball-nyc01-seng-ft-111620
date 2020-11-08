ff898c90e590c3bc22ef89ae002ba1530168421e
Alex Marz <alexhmarz@gmail.com>
2020-11-08 17:46:48 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -63,9 +63,8 @@ def game_hash
   away: {
     team_name: "Charlotte Hornets",
     colors: ["Turquoise", "Purple"],
-    players: {
-     player_name: {
-        "Jeff Adrien" => {
+    players: [
+          player_name:"Jeff Adrien" 
           number: 4,
           shoe: 18,
           points: 10,

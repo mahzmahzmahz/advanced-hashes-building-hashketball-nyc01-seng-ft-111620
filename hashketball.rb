37fceddce8e7339df70b734758218e64de9cfc29
Alex Marz <alexhmarz@gmail.com>
2020-11-08 17:54:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -97,6 +97,18 @@ def game_hash
           blocks: 5,
           slam_dunks: 5,
         },
+        
+        { player_name: "Ben Gordon",
+          number: 8,
+          shoe: 15,
+          points: 33,
+          rebounds: 3,
+          assists: 2,
+          steals: 1,
+          blocks: 1,
+          slam_dunks: 0 
+        },
+        
         { player_name: "Kemba Walker", 
           number: 33,
           shoe: 15,
